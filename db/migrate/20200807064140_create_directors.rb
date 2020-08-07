f7a22f7a7709e3b1da0ea882e0501689302ff9e0
Raghu Betina <raghu@firstdraft.com>
2020-08-07 06:41:41 +0000

    Generate directors resource with ideas:scaffold

---
+++ db/migrate/20200807064140_create_directors.rb
@@ -0,0 +1,11 @@
+class CreateDirectors < ActiveRecord::Migration[6.0]
+  def change
+    create_table :directors do |t|
+      t.string :name
+      t.string :bio
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
