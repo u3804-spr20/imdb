883aab3911153405b60d844176c6c6e5e7ed980e
Raghu Betina <raghu@firstdraft.com>
2020-08-07 06:41:47 +0000

    Validate presence of movies.title

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,6 +5,8 @@ class Movie < ApplicationRecord
 
   # Validations
 
+  validates :title, :presence => true
+
   # Scopes
 
   def to_s
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -12,5 +12,7 @@ RSpec.describe Movie, type: :model do
 
     describe "Validations" do
 
+    it { should validate_presence_of(:title) }
+
     end
 end
