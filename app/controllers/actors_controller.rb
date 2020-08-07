8d6a13691d737157e53816ae98e32c1a1b3c758d
Raghu Betina <raghu@firstdraft.com>
2020-08-07 06:44:39 +0000

    Remove controller comments

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,26 +1,21 @@
 class ActorsController < ApplicationController
   before_action :set_actor, only: %i[show edit update destroy]
 
-  # GET /actors
   def index
     @q = Actor.ransack(params[:q])
     @actors = @q.result(distinct: true).includes(:characters, :movies).page(params[:page]).per(10)
   end
 
-  # GET /actors/1
   def show
     @casting = Casting.new
   end
 
-  # GET /actors/new
   def new
     @actor = Actor.new
   end
 
-  # GET /actors/1/edit
   def edit; end
 
-  # POST /actors
   def create
     @actor = Actor.new(actor_params)
 
@@ -31,7 +26,6 @@ class ActorsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /actors/1
   def update
     if @actor.update(actor_params)
       redirect_to @actor, notice: "Actor was successfully updated."
@@ -40,7 +34,6 @@ class ActorsController < ApplicationController
     end
   end
 
-  # DELETE /actors/1
   def destroy
     @actor.destroy
     redirect_to actors_url, notice: "Actor was successfully destroyed."
@@ -48,12 +41,10 @@ class ActorsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_actor
     @actor = Actor.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def actor_params
     params.require(:actor).permit(:name, :bio, :image, :dob)
   end
--- app/controllers/castings_controller.rb
+++ app/controllers/castings_controller.rb
@@ -1,24 +1,19 @@
 class CastingsController < ApplicationController
   before_action :set_casting, only: %i[show edit update destroy]
 
-  # GET /castings
   def index
     @q = Casting.ransack(params[:q])
     @castings = @q.result(distinct: true).includes(:movie, :actor).page(params[:page]).per(10)
   end
 
-  # GET /castings/1
   def show; end
 
-  # GET /castings/new
   def new
     @casting = Casting.new
   end
 
-  # GET /castings/1/edit
   def edit; end
 
-  # POST /castings
   def create
     @casting = Casting.new(casting_params)
 
@@ -34,7 +29,6 @@ class CastingsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /castings/1
   def update
     if @casting.update(casting_params)
       redirect_to @casting, notice: "Casting was successfully updated."
@@ -43,7 +37,6 @@ class CastingsController < ApplicationController
     end
   end
 
-  # DELETE /castings/1
   def destroy
     @casting.destroy
     message = "Casting was successfully deleted."
@@ -56,12 +49,10 @@ class CastingsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_casting
     @casting = Casting.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def casting_params
     params.require(:casting).permit(:movie_id, :actor_id, :character_name)
   end
