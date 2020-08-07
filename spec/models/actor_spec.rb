4211b9516336b437294fd9bd70bd378448e62047
Raghu Betina <raghu@firstdraft.com>
2020-08-07 06:41:47 +0000

    Validate uniqueness of actors.name

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -12,6 +12,8 @@ RSpec.describe Actor, type: :model do
 
     describe "Validations" do
 
+    it { should validate_uniqueness_of(:name).scoped_to(:dob) }
+
     it { should validate_presence_of(:name) }
 
     end
