0dc4b4fe87773eb5504d4f5eeaf341c6bb2f984e
Raghu Betina <raghu@firstdraft.com>
2020-08-07 06:42:02 +0000

    Display associated collections on show pages

--- app/controllers/castings_controller.rb
+++ app/controllers/castings_controller.rb
@@ -42,8 +42,14 @@ class CastingsController < ApplicationController
   # DELETE /castings/1
   def destroy
     @casting.destroy
-    redirect_to castings_url, notice: 'Casting was successfully destroyed.'
+    message = "Casting was successfully deleted."
+    if Rails.application.routes.recognize_path(request.referrer)[:controller] != Rails.application.routes.recognize_path(request.path)[:controller]
+      redirect_back fallback_location: request.referrer, notice: message
+    else
+      redirect_to castings_url, notice: message
+    end
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
